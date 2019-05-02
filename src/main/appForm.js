54b51cca4878b49e871ede35315313676a7334d6
neozumm <neo.ivan92@gmail.com>
2019-05-02 15:30:05 +0300

    success message fixed

--- src/main/appForm.js
+++ src/main/appForm.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import { Button, Table, Container } from "reactstrap";
 import { socket } from "../global/header";
 
+function SuccessMsg() {
+    return (
+        <p>Успешно отправлено</p>
+    );
+}
+
 class AppForm extends Component{
     constructor(){
         super();
         this.state = {
             name:"",
             theme:"",
-            successMsg:""
+            successState: false
         };
         this.onNameChange = this.onNameChange.bind(this);
         this.onThemeChange = this.onThemeChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-        this.successfullyRecieved = this.successfullyRecieved.bind(this);
+        this.successToggle = this.successToggle.bind(this);
     }
 
     onNameChange(event){
@@ -22,19 +28,23 @@ class AppForm extends Component{
     onThemeChange(event){
         this.setState({theme: event.target.value});
     }
-    onSubmit(){
+    onSubmit(event){
+        event.preventDefault();
         let data = {
             name: this.state.name,
             theme: this.state.theme
         };
         socket.emit('dataSubmit', data);
         socket.on('dataSuccess', ()=>{
-            this.successfullyRecieved();
+            this.successToggle();
+            setTimeout(()=>{
+                this.successToggle();
+            }, 5000);
         })
     }
 
-    successfullyRecieved(){
-        this.setState({successMsg: "Успешно отправлено"});
+    successToggle(){
+        this.setState(prevState=> ({successState: !prevState.successState}));
     }
 
     render() {
@@ -46,7 +56,7 @@ class AppForm extends Component{
                     <input type="text" onChange={this.onThemeChange} className="theme" placeholder="Тема работы"/>
                     <input type="submit" value="Отправить" />
                 </form>
-                <p>{this.state.successMsg}</p>
+                {this.state.successState ? <SuccessMsg /> : null}
             </Container>
         );
     }
