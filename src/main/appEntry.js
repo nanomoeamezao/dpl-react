97e5ea6ec775e8ec6f0812d34ea0cd0857ce8b73
neozumm <neo.ivan92@gmail.com>
2019-05-03 23:42:48 +0300

    deleted trash files appEntry select and onchange event setup

--- src/main/appEntry.js
+++ src/main/appEntry.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, Table, Container } from "reactstrap";
 import { socket } from "../global/header";
 
+
 class AppEntry extends Component{
     constructor(props){
         super(props);
@@ -13,7 +14,8 @@ class AppEntry extends Component{
         };
     }
 
-    updateApp = data=>{
+
+    loadApp = data=>{
         this.setState({id: data[0].id, name: data[0].name, theme: data[0].theme, status: data[0].status});
         console.log("recieved application data, testing new state: " + this.state.name);
         console.log(data[0]);
@@ -22,14 +24,14 @@ class AppEntry extends Component{
     componentDidMount(props){
         socket.emit("reqApp", this.props.match.params.id);
         console.log("sending applications request with id: "+ this.props.match.params.id);
-        socket.on("resApp", this.updateApp)
+        socket.on("resApp", this.loadApp)
     }
 
     componentWillUnmount(){
         socket.off("resApp");
     }
 
-    insertApplication(){
+    /*insertApplication(){
         return(
             <div>
                 <p> Имя участника: {this.state.name} </p>
@@ -39,17 +41,56 @@ class AppEntry extends Component{
             </div>
 
         );
-    }
+    }*/
 
     render() {
+        var d=this.state;
         return (
             <Container>
                 <div>
                     <h1>TESTING PROPS {this.props.match.params.id} </h1>
-                    <p>{this.insertApplication()}</p>
+                    {this.state.id!='' ? <Application data={d}/> : null}
                 </div>
             </Container>
         );
     }
 }
+
+class Application extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            id: props.data.id,
+            name: props.data.name,
+            theme: props.data.theme,
+            status: props.data.status
+        };
+    }
+    updateStatus= event => {
+        var msg = {
+            id: this.state.id,
+            status: event.target.value
+        };
+        socket.emit("updateStatus", msg);
+        console.log("status update sent");
+    };
+    render(){
+        return(
+            <Container>
+                <div>
+                    <p> Имя участника: {this.state.name} </p>
+                    <p> Тема работы: {this.state.theme} </p>
+                    <p> ID работы: {this.state.id} </p>
+                    <p> Статус работы:
+                        <select onChange={this.updateStatus} size="1">
+                            <option selected  value={this.state.status}>{this.state.status}</option>
+                            <option value={this.state.status=='not approved' ? 'approved' : 'approved'}>{this.state.status=='approved' ? 'not approved' : 'approved'}</option>
+                        </select>
+                    </p>
+                </div>
+            </Container>
+        )
+    }
+}
+
 export default AppEntry;
