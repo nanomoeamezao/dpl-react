44db39b22c9d6f97baec3691f45e1cbc68810f8c
neozumm <neozumm@rambler.ru>
2020-05-22 16:08:04 +0300

    debug change delete

--- src/global/header.js
+++ src/global/header.js
@@ -10,7 +10,7 @@ class Header extends Component {
         this.state = {
             endpoint: 'http://localhost:3001/'
         };
-        socket = socketIOClient(this.state.endpoint);
+        socket = socketIOClient();
     }
     render() {
         return (
