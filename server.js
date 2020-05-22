49e8cccfcf1d4c22727daf1a7881b2c4fbbc1b49
neozumm <neozumm@rambler.ru>
2020-05-22 14:00:28 +0300

    port fix attemp

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-let server = app.listen(port);
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static(__dirname));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+
+let server = app.listen(port);
 let sio = require('socket.io').listen(server);
-
-
 sio.on('connect', (socket)=> {
     socket.on("dataRequest", function () {
         console.log("recieved dataRequest");
