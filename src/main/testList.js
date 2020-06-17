ec7d25e78be28143958104a9ffb8e574f3d8f6db
neozumm <neozumm@rambler.ru>
2020-06-18 00:45:07 +0300

    refactor

--- src/main/testList.js
+++ src/main/testList.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { connect } from "react-redux";
-const mapStateToProps = state => {
-  console.log(state)
-  return { test: state.testReducer.test };
-};
+import React from 'react'
+import { connect } from 'react-redux'
+const mapStateToProps = (state) => {
+    console.log(state)
+    return { test: state.testReducer.test }
+}
 const ConnectedList = ({ test }) => (
-  <div>
-    <p>Test list</p>
-    <ul>
-      {test.map(el => (
-        <li key={el.id}>{el.title}</li>
-      ))}
-    </ul>
-  </div>
-);
-const TestList = connect(mapStateToProps)(ConnectedList);
+    <div>
+        <p>Test list</p>
+        <ul>
+            {test.map((el) => (
+                <li key={el.id}>{el.title}</li>
+            ))}
+        </ul>
+    </div>
+)
+const TestList = connect(mapStateToProps)(ConnectedList)
 export default TestList
